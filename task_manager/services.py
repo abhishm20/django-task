70d864abe9f676b9f34018d3e8f7d2e38c9388ef
Abhishek Sharma <abhishm20@gmail.com>
2023-02-09 12:13:48 +0530

    Initial Commit

--- task_manager/services.py
+++ task_manager/services.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import uuid
+
 from django_extra.core.services import BaseService
 
 from .models import Task
@@ -8,3 +10,8 @@ from .serializers import TaskSerializer
 class TaskService(BaseService):
     serializer = TaskSerializer
     model = Task
+
+    def create(self, data):
+        if not data.get("identifier"):
+            data["identifier"] = str(uuid.uuid4())
+        return super().create(data)
